9bbc929037e41d872891db9f112118ee3bfbb127
Naveenkms <nvnkms123@gmail.com>
2026-01-12 14:32:26 +0530

    fix: history page overflowing beyond the screen height

--- app/dashboard/history/page.tsx
+++ app/dashboard/history/page.tsx
@@ -6,7 +6,7 @@ import DataTableSkeleton from "./_components/data-table-skeleton";
 
 export default async function HistoryPage() {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full h-full flex flex-col">
       <PageHeader>History</PageHeader>
       <div className="py-4 px-4 md:px-6 flex-1 overflow-auto">
         <Suspense fallback={<DataTableSkeleton />}>
