45df033ca8515c0b8a08ad1d30a89493d88acadb
Naveen Krishna M S <89766436+Naveenkms@users.noreply.github.com>
2026-03-15 21:40:57 +0530

    Log userId header in POST request

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -4,6 +4,8 @@ export async function POST(request: NextRequest) {
 
   const res = await request.json();
   console.log("log: ", res);
+  console.log("header:", request.headers.get("userId"))
+
 
   return NextResponse.json({ message: "success", data: res });
 }
